dc2b473569197758449b0de34cb0f63f71faf5de
Shubham <shubham.bhandarkar03@gmail.com>
2026-05-05 07:45:32 +0530

    Add TensorFlow warning suppression to GUI entry point

--- steg_hunter_gui.py
+++ steg_hunter_gui.py
@@ -1,7 +1,16 @@
 """
 StegHunter GUI Application Entry Point
 """
+import os
+import warnings
 import sys
+
+# Suppress TensorFlow and oneDNN warnings before importing anything that uses TF
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+warnings.filterwarnings('ignore', category=DeprecationWarning)
+warnings.filterwarnings('ignore', category=FutureWarning)
+
 from PyQt5.QtWidgets import QApplication
 from src.gui.main_window import MainWindow
 from src.gui.professional_style import apply_professional_style, load_theme_preference
